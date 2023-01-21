27bbb0878842a5ef9208c805387e1a578523d747
ivan retamozo <retamozoi445@gmail.com>
2023-01-20 23:49:19 -0300

    modelo de tickets

--- public/js/socket-client.js
+++ public/js/socket-client.js
@@ -1,33 +1,47 @@
-//DOM REFERS
-const lblOnline = document.querySelector("#lblOnline");
-const lblOfline = document.querySelector("#lblOfline");
-const txtMessage = document.querySelector("#txtMessage");
-const btnSend = document.querySelector("#btnSend");
+
+// Referencias del HTML
+const lblOnline  = document.querySelector('#lblOnline');
+const lblOffline = document.querySelector('#lblOffline');
+const txtMensaje = document.querySelector('#txtMensaje');
+const btnEnviar  = document.querySelector('#btnEnviar');
+
 
 const socket = io();
 
-socket.on("connect", () => {
-  lblOfline.style.display = "none";
-  lblOnline.style.display = "";
+
+
+socket.on('connect', () => {
+    // console.log('Conectado');
+
+    lblOffline.style.display = 'none';
+    lblOnline.style.display  = '';
+
 });
 
-socket.on("disconnect", () => {
-  lblOnline.style.display = "none";
-  lblOfline.style.display = "";
+socket.on('disconnect', () => {
+    // console.log('Desconectado del servidor');
+
+    lblOnline.style.display  = 'none';
+    lblOffline.style.display = '';
 });
 
-socket.on("send-message", (payload) => {
-  console.log(payload);
-});
 
-btnSend.addEventListener("click", () => {
-  const message = txtMessage.value;
-  const payload = {
-    message,
-    id: "asdsdka9sd",
-    date: new Date().getTime(),
-  };
-  socket.emit("send-message", payload, (id) => {
-    console.log("desde el server", id);
-  });
+socket.on('enviar-mensaje', (payload) => {
+    console.log( payload )
+})
+
+
+btnEnviar.addEventListener( 'click', () => {
+
+    const mensaje = txtMensaje.value;
+    const payload = {
+        mensaje,
+        id: '123ABC',
+        fecha: new Date().getTime()
+    }
+    
+    socket.emit( 'enviar-mensaje', payload, ( id ) => {
+        console.log('Desde el server', id );
+    });
+
 });
--- sockets/controllers.js
+++ sockets/controllers.js
@@ -1,3 +1,7 @@
+const TicketControl = require("../models/ticket-control");
+
+const ticketControl = new TicketControl();
+
 const socketControllers = (socket) => {
   socket.on("send-message", (payload, callback) => {
     const id = 12345;
